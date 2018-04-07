ea1f37ec9627f91843b08c1de1185e736e508f9e
Bundyclan <bundyclantube@gmail.com>
2018-04-07 18:12:42 +0200

    database for login

--- check_login.php
+++ check_login.php
@@ -1,8 +1,28 @@
 <?php
 
+//DATABASE CONNECTION
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "youconnect";
+
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+//Getting user data
+
+
+
+
+
+//withabigDaria said i should  document this
+
+
+// user data received from login
+
 $userName = $_POST['txtUserName'];
 $userPass = $_POST['txtUserPassword'];
 
+// temporary placeholder for correct login
 $correctUserName = "Adam";
 $correctPassword = "123";
 
