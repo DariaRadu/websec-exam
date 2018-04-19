57f4d3140cb64522d5ec24eec32fdce603ccf2f2
Daria Radu <dariaradur3@gmail.com>
2018-04-19 19:00:03 +0200

    include file updated + base folders

--- include.php
+++ include.php
@@ -10,6 +10,7 @@ function gen_header() {
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
              <!-- Compiled and minified CSS -->
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+            <link rel="stylesheet" href="css/style.css">
             <title>YouConnect</title>
         </head>
         <body>
@@ -25,7 +26,9 @@ function nav(){
 function footer(){
     ?>
         <!-- Compiled and minified JavaScript -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+        <script src="js/main.js"></script>
         </body>
     </html>
     <?php
